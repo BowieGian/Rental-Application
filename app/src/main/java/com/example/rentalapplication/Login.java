550134e06717d424ea0577b9d0bf36efa3cd2d0d
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 19:46:53 -0800

    Added checking credentials to Login button. Every time the Login screen is opened, a List of UserAccounts is updated.

--- app/src/main/java/com/example/rentalapplication/Login.java
+++ app/src/main/java/com/example/rentalapplication/Login.java
@@ -1,24 +1,40 @@
 package com.example.rentalapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Login extends AppCompatActivity {
     private FirebaseDatabase rootNode;
     private DatabaseReference reference;
+    List<UserAccount> userAccountList;
+    private TextView usernameText;
+    private TextView passwordText;
 
     public void loginOnClick(View view){
         //execute selectOption() only if the login is valid otherwise display error
-        selectOption();
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        for (UserAccount userAccount : userAccountList) {
+            if (userAccount.getUserName().equals(username) && userAccount.getPassword().equals(password))
+                selectOption();
+        }
+        //display username & password do not match
     }
 
     public void selectOption(){
@@ -42,12 +58,37 @@ public class Login extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        reference.orderByChild("userName").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userAccountList.clear();
+
+                for(DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    UserAccount userAccount = userSnapshot.getValue(UserAccount.class);
+
+                    userAccountList.add(userAccount);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         signUp();
         rootNode = FirebaseDatabase.getInstance();
         reference = rootNode.getReference("Seller");
-
+        userAccountList = new ArrayList<>();
+        usernameText = (EditText)findViewById(R.id.editTextTextPersonName);
+        passwordText = (EditText)findViewById(R.id.editTextTextPassword);
     }
 }
