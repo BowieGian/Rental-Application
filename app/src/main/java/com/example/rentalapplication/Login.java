510dca87dd0e4ec8315be780fa2c8a1de7c9ac88
BowieGian <bowieg@hotmail.com>
2020-12-06 15:59:05 -0800

    Merged everything together

--- app/src/main/java/com/example/rentalapplication/Login.java
+++ app/src/main/java/com/example/rentalapplication/Login.java
@@ -13,22 +13,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import java.util.ArrayList;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
